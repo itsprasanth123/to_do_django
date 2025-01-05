997fc179cfd797e8a1ed1fc398e74ec1746d7b07
Aswin <achu322001.com>
2025-01-05 16:57:24 +0530

    15:15

--- base/models.py
+++ base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import user
 # Create your models here.
+
+class Task(models.model):
+    ures=models.ForeignKey(user, on_delete=models.CASCADE, null=true, blank=true)
+    title=models.charfield(max_length=200)
+    description=models.textfield(null=true, blank=true)
+    complete=models.Booleanfield(default=false)
+    create=models.datatimefield(auto_naw_add=true)
